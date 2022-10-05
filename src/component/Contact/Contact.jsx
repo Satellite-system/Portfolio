519f07cd8c763aeb74c4d585c9ade6f30ec3e1cf
adarsh <adarshurmaliya@gmail.com>
2022-10-05 23:06:05 +0530

    Add Email functionality to Contact Component

--- src/component/Contact/Contact.jsx
+++ src/component/Contact/Contact.jsx
@@ -1,7 +1,46 @@
-import React from 'react'
+import React,{useRef, useState} from 'react'
 import './Contact.css'
+import emailjs from '@emailjs/browser';
 
 function Contact() {
+   const form = useRef();
+   const [send, setSend] = useState(false);
+   const [name, setName] = useState('');
+   const [email, setEmail] = useState('');
+   const [msg, setMsg] = useState('');
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    if(msg==='' || name==='' || email==''){
+      console.log("empty");
+    } 
+    else{
+      console.log("Name", name,"\nEmail: ", email,"\nMsg: ",msg);
+      emailjs.sendForm('service_z3g0s0d', 'template_wwed1vc', form.current, 'yNf9DbrSYQkZjZrd3')
+         .then((result) => {
+            console.log(result.text);
+            setSend(true); 
+            setEmail('');
+            setName('');
+            setMsg('');
+         }, (error) => {
+            console.log(error.text);
+         });
+      
+   }
+  };
+
+  const handleNameChange = (e)=>{
+      setName(e.target.value);
+  }
+  const handleEmailChange = (e)=>{
+      setEmail(e.target.value);
+  }
+  const handleMsgChange = (e)=>{
+      setMsg(e.target.value);
+  }
+
   return (
     <div className="contact">
       <div className="contact-left">
@@ -12,11 +51,12 @@ function Contact() {
          </div>
       </div>
       <div className="contact-right">
-         <form >
-            <input type="text" name='user_name' className='user' placeholder='Name'/>
-            <input type="email" name='user_email' className='user' placeholder='Email' />
-            <textarea name='message' className='user' placeholder='Message' />
+         <form ref={form} onSubmit={sendEmail}>
+            <input type="text" value={name} onChange={handleNameChange} name='from_name' className='user' placeholder='Name'/>
+            <input type="email" value={email} onChange={handleEmailChange} name='user_email' className='user' placeholder='Email' />
+            <textarea name='message' value={msg} onChange={handleMsgChange} className='user' placeholder='Message' />
             <input type='submit' value='Send' className="btn" />
+            <span>{send && "Thanks for Contacting me."}</span>
             <div className="blur contact-blur1"></div>
          </form>
       </div>
