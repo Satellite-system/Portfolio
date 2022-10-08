b72033964861043a9122a6d02397ebb0947901b1
adarsh <adarshurmaliya@gmail.com>
2022-10-09 03:28:07 +0530

    Add Responsive Styles

--- src/component/Portfolio/Portfolio.jsx
+++ src/component/Portfolio/Portfolio.jsx
@@ -7,19 +7,20 @@ import hoc from '../../img/hoc.png'
 import musicapp from '../../img/musicapp.png'
 import 'swiper/css'
 import { themeContext } from '../../Context'
-import { Autoplay } from 'swiper'
 
-function Portfolio() {
+export default function Portfolio() {
    const theme = useContext(themeContext);
    const darkMode = theme.state.darkMode;
+   const {innerWidth, innerHeight} = window;
+
   return (
-    <div className="porfolio">
+    <div className="porfolio" id='Portfolio'>
          {/* Heading */}
          <span style={darkMode?{color:"#ffffff"}:{}}>Recent Projects</span>
          <span>Portfolio</span>
 
          {/* Slider */}
-         <Swiper className='porfolio-slider' spaceBetween={30} slidesPerView={3} grabCursor={true} loop={true} >
+         <Swiper className='porfolio-slider' spaceBetween={{innerWidth}>480?30:100}  slidesPerView={innerWidth>480?3:1} grabCursor={true} loop={true} >
             <SwiperSlide>
                <img src={sidebar} alt="" />
             </SwiperSlide>
@@ -38,4 +39,3 @@ function Portfolio() {
   )
 }
 
-export default Portfolio
